fc833beddcbe93736121e0a0ea08750f7cc790b6
Dhameliyahit <153006638+dhameliyahit@users.noreply.github.com>
2024-09-30 16:43:43 +0530

    add json middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const connectDB = require("./DB/connectDB.js")
 const PORT = process.env.PORT || 5000
 
 connectDB(process.env.DB_URL)
+
+app.use(express.json())
+
 const app = express()
 
 app.use("/api/v1",contactRoute)
